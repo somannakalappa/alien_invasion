6f3a2c2cf79c5a67c7bb7485f2841e1ddab6de2f
kjbryantdrew <kjbryantdrew@gmail.com>
2018-01-19 15:50:07 +0800

    alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,7 +5,7 @@ import pygame
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((1000, 600))
     pygame.display.set_caption("Alien Invasion")
 
     # 开始游戏的主循环
