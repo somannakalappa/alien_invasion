5df16dac340e1d96afaf66254840b9c95566938e
kjbryantdrew <kjbryantdrew@gmail.com>
2018-01-19 17:03:48 +0800

    alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,10 +2,12 @@ import sys
 
 import pygame
 
+from settings import Settings
 def run_game():
-    # 初始化游戏并创建一个屏幕对象
+    # 初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1000, 600))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # 开始游戏的主循环
@@ -15,6 +17,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环都重绘屏幕
+        screen.fill(ai_settings.bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
